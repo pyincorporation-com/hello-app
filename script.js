39b5822b4f854398f97bdd742f283e9ba5cede40
gabriel nzilantuzu <gabrielnzilantuzu@gmail.com>
2024-11-16 09:50:42 +0300

    added all files

---
+++ script.js
@@ -0,0 +1,11 @@
+// Show the modal
+function showModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "flex";
+}
+
+// Close the modal
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
